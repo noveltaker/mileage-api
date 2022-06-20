5a2b9d94320fbfb770b164971f78b2ef2357bd84
noveltaker <pnw123456@icloud.com>
2022-06-20 09:55:53 +0900

    first commit

---
+++ src/main/java/com/example/mileageapi/MileageApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mileageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MileageApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MileageApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mileageapi/MileageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mileageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MileageApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
