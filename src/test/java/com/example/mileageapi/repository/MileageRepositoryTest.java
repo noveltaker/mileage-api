4f617055af1dadb9c2c2118d947ecb342baefc7f
noveltaker <pnw123456@icloud.com>
2022-06-23 18:56:54 +0900

    test: 리뷰 아이디 별로 데이터 조회 테스트 케이스

--- src/test/java/com/example/mileageapi/repository/MileageRepositoryTest.java
+++ src/test/java/com/example/mileageapi/repository/MileageRepositoryTest.java
@@ -1,10 +1,9 @@
 package com.example.mileageapi.repository;
 
 import com.example.mileageapi.domain.Mileage;
+import com.example.mileageapi.mock.EventDTOMock;
 import com.example.mileageapi.mock.MileageMock;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -29,13 +28,9 @@ class MileageRepositoryTest {
 
         List<Mileage> entities = mileageRepository.saveAll(mocks);
 
-
         for (Mileage mock : mocks) {
 
-            Mileage entity = entities.stream()
-                    .filter(foundEntity -> foundEntity.getType().equals(mock.getType()))
-                    .findFirst()
-                    .orElseThrow();
+            Mileage entity = entities.stream().filter(foundEntity -> foundEntity.getType().equals(mock.getType())).findFirst().orElseThrow();
 
             Assertions.assertEquals(mock.getType(), entity.getType());
             Assertions.assertEquals(mock.getReviewId(), entity.getReviewId());
@@ -44,6 +39,37 @@ class MileageRepositoryTest {
             Assertions.assertEquals(mock.getPoint(), entity.getPoint());
 
         }
+    }
 
+    @Nested
+    @DisplayName("조회")
+    class Select {
+
+        private List<Mileage> mocks;
+
+        @BeforeEach
+        void init() {
+            mocks = mileageRepository.saveAll(MileageMock.createdMocks());
+            mileageRepository.flush();
+        }
+
+        @Test
+        @DisplayName("리뷰 아이디 별로 데이터 조회 테스트 케이스")
+        void findByReviewId() {
+
+            List<Mileage> entities = mileageRepository.findByReviewId(EventDTOMock.reviewId);
+
+            for (Mileage mock : mocks) {
+
+                Mileage entity = entities.stream().filter(foundEntity -> foundEntity.getType().equals(mock.getType())).findFirst().orElseThrow();
+
+                Assertions.assertEquals(mock.getType(), entity.getType());
+                Assertions.assertEquals(mock.getReviewId(), entity.getReviewId());
+                Assertions.assertEquals(mock.getPlaceId(), entity.getPlaceId());
+                Assertions.assertEquals(mock.getUserId(), entity.getUserId());
+                Assertions.assertEquals(mock.getPoint(), entity.getPoint());
+
+            }
+        }
     }
 }
