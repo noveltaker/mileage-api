720a4225ec6368b834bbb6781761f1a4fa51becb
noveltaker <pnw123456@icloud.com>
2022-06-26 23:18:49 +0900

    test: 유저별 마일리지 히스토리 리스트 조회 테스트 케이스

--- src/test/java/com/example/mileageapi/repository/MileageHistoryRepositoryTest.java
+++ src/test/java/com/example/mileageapi/repository/MileageHistoryRepositoryTest.java
@@ -5,13 +5,17 @@ import com.example.mileageapi.domain.MileageHistory;
 import com.example.mileageapi.mock.EventDTOMock;
 import com.example.mileageapi.mock.MileageHistoryMock;
 import com.example.mileageapi.mock.MileageMock;
+import com.example.mileageapi.service.dto.MileageHistoryInfo;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+import java.util.UUID;
 
 @DataJpaTest
 @ExtendWith(SpringExtension.class)
@@ -22,6 +26,8 @@ class MileageHistoryRepositoryTest {
   @Autowired private MileageRepository mileageRepository;
 
   private Mileage mileage;
+
+  private UUID mockUserId = EventDTOMock.userId;
 
   @BeforeEach
   void init() {
@@ -60,8 +66,7 @@ class MileageHistoryRepositoryTest {
 
     @BeforeEach
     void init() {
-      mocks = mileageHistoryRepository.saveAll(MileageHistoryMock.createdMocks(mileage));
-      mileageHistoryRepository.flush();
+      mocks = mileageHistoryRepository.saveAllAndFlush(MileageHistoryMock.createdMocks(mileage));
     }
 
     @Test
@@ -76,6 +81,37 @@ class MileageHistoryRepositoryTest {
         MileageHistory entity =
             entities.stream()
                 .filter(foundEntity -> foundEntity.getType().equals(mock.getType()))
+                .findFirst()
+                .orElseThrow();
+
+        Assertions.assertEquals(mock.getType(), entity.getType());
+        Assertions.assertEquals(mock.getReviewId(), entity.getReviewId());
+        Assertions.assertEquals(mock.getPlaceId(), entity.getPlaceId());
+        Assertions.assertEquals(mock.getPoint(), entity.getPoint());
+      }
+    }
+
+    @Test
+    @DisplayName("유저별 마일리지 히스토리 리스트 조회")
+    void findByMileage_UserId() {
+
+      PageRequest pageable = PageRequest.of(0, 10);
+
+      Page<MileageHistoryInfo> pageEntities =
+          mileageHistoryRepository.findByMileage_UserId(
+              pageable, mockUserId, MileageHistoryInfo.class);
+
+      Assertions.assertFalse(pageEntities.isEmpty());
+
+      List<MileageHistoryInfo> entities = pageEntities.getContent();
+
+      Assertions.assertEquals(entities.size(), mocks.size());
+
+      for (MileageHistory mock : mocks) {
+
+        MileageHistoryInfo entity =
+            entities.stream()
+                .filter(value -> value.getId().equals(mock.getId()))
                 .findFirst()
                 .orElseThrow();
 
