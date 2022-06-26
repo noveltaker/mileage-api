5df0e8020dd3fe05443e0aff3844e29f2fce7988
noveltaker <pnw123456@icloud.com>
2022-06-26 12:01:08 +0900

    test: mileage 아이디 별로 조회 테스트 케이스

---
+++ src/test/java/com/example/mileageapi/repository/MileageRepositoryTest.java
@@ -0,0 +1,60 @@
+package com.example.mileageapi.repository;
+
+import com.example.mileageapi.domain.Mileage;
+import com.example.mileageapi.domain.MileageHistory;
+import com.example.mileageapi.mock.EventDTOMock;
+import com.example.mileageapi.mock.MileageHistoryMock;
+import com.example.mileageapi.mock.MileageMock;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+class MileageRepositoryTest {
+
+  @Autowired private MileageRepository mileageRepository;
+
+  @Autowired private MileageHistoryRepository mileageHistoryRepository;
+
+  @Nested
+  @DisplayName("조회")
+  class Select {
+
+    private Mileage mock;
+
+    private List<MileageHistory> histories;
+
+    private UUID mockUserId = EventDTOMock.userId;
+
+    @BeforeEach
+    void init() {
+
+      mock = mileageRepository.save(MileageMock.createdMock());
+
+      mileageRepository.flush();
+
+      histories = mileageHistoryRepository.saveAll(MileageHistoryMock.createdMocks(mock));
+
+      mileageHistoryRepository.flush();
+    }
+
+    @Test
+    @DisplayName("아이디 별로 조회 테스트 케이스")
+    void findById() {
+
+      Mileage entity = mileageRepository.findById(mockUserId).orElseThrow();
+
+      Assertions.assertEquals(mock.getUserId(), entity.getUserId());
+      Assertions.assertEquals(mock.getPoint(), entity.getPoint());
+    }
+  }
+}
