a40d5fa90daed6a6083bd97cccb78851c7fc6fff
noveltaker <pnw123456@icloud.com>
2022-06-26 11:51:42 +0900

    test: mileage 추가로인한 테스트 케이스 변경

--- src/test/java/com/example/mileageapi/mock/MileageHistoryMock.java
+++ src/test/java/com/example/mileageapi/mock/MileageHistoryMock.java
@@ -1,45 +1,45 @@
 package com.example.mileageapi.mock;
 
 import com.example.mileageapi.constants.MileageHistoryType;
+import com.example.mileageapi.domain.Mileage;
 import com.example.mileageapi.domain.MileageHistory;
 
 import java.util.List;
 
 public class MileageHistoryMock {
 
-    public static MileageHistory createdMock() {
-        return MileageHistory.builder()
-                .type(MileageHistoryType.REVIEW_ADD)
-                .reviewId(EventDTOMock.reviewId)
-                .placeId(EventDTOMock.placeId)
-//                .userId(EventDTOMock.userId)
-                .point(1)
-                .build();
-    }
+  public static MileageHistory createdMock(Mileage mileage) {
+    return MileageHistory.builder()
+        .type(MileageHistoryType.REVIEW_ADD)
+        .reviewId(EventDTOMock.reviewId)
+        .placeId(EventDTOMock.placeId)
+        .mileage(mileage)
+        .point(1)
+        .build();
+  }
 
-    public static List<MileageHistory> createdMocks() {
-        return List.of(
-                MileageHistory.builder()
-                        .type(MileageHistoryType.REVIEW_ADD)
-                        .reviewId(EventDTOMock.reviewId)
-                        .placeId(EventDTOMock.placeId)
-//                        .userId(EventDTOMock.userId)
-                        .point(1)
-                        .build(),
-                MileageHistory.builder()
-                        .type(MileageHistoryType.CONTENT_ADD)
-                        .reviewId(EventDTOMock.reviewId)
-                        .placeId(EventDTOMock.placeId)
-//                        .userId(EventDTOMock.userId)
-                        .point(1)
-                        .build(),
-                MileageHistory.builder()
-                        .type(MileageHistoryType.PHOTO_ADD)
-                        .reviewId(EventDTOMock.reviewId)
-                        .placeId(EventDTOMock.placeId)
-//                        .userId(EventDTOMock.userId)
-                        .point(1)
-                        .build()
-        );
-    }
+  public static List<MileageHistory> createdMocks(Mileage mileage) {
+    return List.of(
+        MileageHistory.builder()
+            .type(MileageHistoryType.REVIEW_ADD)
+            .reviewId(EventDTOMock.reviewId)
+            .placeId(EventDTOMock.placeId)
+            .point(1)
+            .mileage(mileage)
+            .build(),
+        MileageHistory.builder()
+            .type(MileageHistoryType.CONTENT_ADD)
+            .reviewId(EventDTOMock.reviewId)
+            .placeId(EventDTOMock.placeId)
+            .point(1)
+            .mileage(mileage)
+            .build(),
+        MileageHistory.builder()
+            .type(MileageHistoryType.PHOTO_ADD)
+            .reviewId(EventDTOMock.reviewId)
+            .placeId(EventDTOMock.placeId)
+            .point(1)
+            .mileage(mileage)
+            .build());
+  }
 }
--- src/test/java/com/example/mileageapi/repository/MileageHistoryRepositoryTest.java
+++ src/test/java/com/example/mileageapi/repository/MileageHistoryRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.example.mileageapi.repository;
 
+import com.example.mileageapi.domain.Mileage;
 import com.example.mileageapi.domain.MileageHistory;
 import com.example.mileageapi.mock.EventDTOMock;
 import com.example.mileageapi.mock.MileageHistoryMock;
+import com.example.mileageapi.mock.MileageMock;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,59 +17,74 @@ import java.util.List;
 @ExtendWith(SpringExtension.class)
 class MileageHistoryRepositoryTest {
 
-    @Autowired
-    private MileageHistoryRepository mileageHistoryRepository;
+  @Autowired private MileageHistoryRepository mileageHistoryRepository;
+
+  @Autowired private MileageRepository mileageRepository;
+
+  private Mileage mileage;
+
+  @Test
+  @DisplayName("초기 세팅")
+  void init() {
+    mileage = mileageRepository.save(MileageMock.createdMock());
+    mileageRepository.flush();
+  }
+
+  @Test
+  @DisplayName("마일리지 포인트들 저장 테스트 케이스")
+  void save() {
+
+    List<MileageHistory> mocks = MileageHistoryMock.createdMocks(mileage);
+
+    List<MileageHistory> entities = mileageHistoryRepository.saveAll(mocks);
+
+    for (MileageHistory mock : mocks) {
+
+      MileageHistory entity =
+          entities.stream()
+              .filter(foundEntity -> foundEntity.getType().equals(mock.getType()))
+              .findFirst()
+              .orElseThrow();
+
+      Assertions.assertEquals(mock.getType(), entity.getType());
+      Assertions.assertEquals(mock.getReviewId(), entity.getReviewId());
+      Assertions.assertEquals(mock.getPlaceId(), entity.getPlaceId());
+      Assertions.assertEquals(mock.getPoint(), entity.getPoint());
+    }
+  }
+
+  @Nested
+  @DisplayName("조회")
+  class Select {
+
+    private List<MileageHistory> mocks;
+
+    @BeforeEach
+    void init() {
+      mocks = mileageHistoryRepository.saveAll(MileageHistoryMock.createdMocks(mileage));
+      mileageHistoryRepository.flush();
+    }
 
     @Test
-    @DisplayName("마일리지 포인트들 저장 테스트 케이스")
-    void save() {
+    @DisplayName("리뷰 아이디 별로 데이터 조회 테스트 케이스")
+    void findByReviewId() {
 
-        List<MileageHistory> mocks = MileageHistoryMock.createdMocks();
+      List<MileageHistory> entities =
+          mileageHistoryRepository.findByReviewId(EventDTOMock.reviewId);
 
-        List<MileageHistory> entities = mileageHistoryRepository.saveAll(mocks);
+      for (MileageHistory mock : mocks) {
 
-        for (MileageHistory mock : mocks) {
+        MileageHistory entity =
+            entities.stream()
+                .filter(foundEntity -> foundEntity.getType().equals(mock.getType()))
+                .findFirst()
+                .orElseThrow();
 
-            MileageHistory entity = entities.stream().filter(foundEntity -> foundEntity.getType().equals(mock.getType())).findFirst().orElseThrow();
-
-            Assertions.assertEquals(mock.getType(), entity.getType());
-            Assertions.assertEquals(mock.getReviewId(), entity.getReviewId());
-            Assertions.assertEquals(mock.getPlaceId(), entity.getPlaceId());
-            Assertions.assertEquals(mock.getUserId(), entity.getUserId());
-            Assertions.assertEquals(mock.getPoint(), entity.getPoint());
-
-        }
+        Assertions.assertEquals(mock.getType(), entity.getType());
+        Assertions.assertEquals(mock.getReviewId(), entity.getReviewId());
+        Assertions.assertEquals(mock.getPlaceId(), entity.getPlaceId());
+        Assertions.assertEquals(mock.getPoint(), entity.getPoint());
+      }
     }
-
-    @Nested
-    @DisplayName("조회")
-    class Select {
-
-        private List<MileageHistory> mocks;
-
-        @BeforeEach
-        void init() {
-            mocks = mileageHistoryRepository.saveAll(MileageHistoryMock.createdMocks());
-            mileageHistoryRepository.flush();
-        }
-
-        @Test
-        @DisplayName("리뷰 아이디 별로 데이터 조회 테스트 케이스")
-        void findByReviewId() {
-
-            List<MileageHistory> entities = mileageHistoryRepository.findByReviewId(EventDTOMock.reviewId);
-
-            for (MileageHistory mock : mocks) {
-
-                MileageHistory entity = entities.stream().filter(foundEntity -> foundEntity.getType().equals(mock.getType())).findFirst().orElseThrow();
-
-                Assertions.assertEquals(mock.getType(), entity.getType());
-                Assertions.assertEquals(mock.getReviewId(), entity.getReviewId());
-                Assertions.assertEquals(mock.getPlaceId(), entity.getPlaceId());
-                Assertions.assertEquals(mock.getUserId(), entity.getUserId());
-                Assertions.assertEquals(mock.getPoint(), entity.getPoint());
-
-            }
-        }
-    }
+  }
 }
