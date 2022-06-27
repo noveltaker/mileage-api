8183747bedb5aab60e49703d684f33324236e0af
noveltaker <pnw123456@icloud.com>
2022-06-27 14:28:47 +0900

    test: 나의 포인트 히스트리 API

--- src/test/java/com/example/mileageapi/web/MileageControllerTest.java
+++ src/test/java/com/example/mileageapi/web/MileageControllerTest.java
@@ -1,8 +1,10 @@
 package com.example.mileageapi.web;
 
 import com.example.mileageapi.mock.EventDTOMock;
+import com.example.mileageapi.mock.MileageHistoryMock;
 import com.example.mileageapi.mock.MileageMock;
 import com.example.mileageapi.service.MileageService;
+import com.example.mileageapi.service.dto.MileageHistoryInfo;
 import com.example.mileageapi.service.dto.MileageInfo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -70,5 +73,46 @@ class MileageControllerTest {
         .andExpect(status().isOk())
         .andExpect(jsonPath("$['userId']").value(mockUserId))
         .andExpect(jsonPath("$['point']").value(point));
+  }
+
+  @Test
+  @DisplayName("나의 포인트 히스트리 API")
+  void getMyMileageHistories_success() throws Exception {
+
+    Page<MileageHistoryInfo> mocks =
+        MileageHistoryMock.getPageMileageHistoryInfoList(MileageMock.createdMock());
+
+    BDDMockito.given(mileageService.getMyMileageHistories(any())).willReturn(mocks);
+
+    ResultActions action =
+        mockMvc
+            .perform(
+                MockMvcRequestBuilders.get("/my-mileage-histories")
+                    .param("userId", EventDTOMock.userId.toString())
+                    .param("page", "0")
+                    .param("size", "10")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .characterEncoding("UTF-8"))
+            .andDo(print());
+
+    BDDMockito.then(mileageService).should().getMyMileageHistories(any());
+
+    action
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$['content'][0]['id']").value(mocks.getContent().get(0).getId()))
+        .andExpect(
+            jsonPath("$['content'][0]['type']").value(mocks.getContent().get(0).getType().name()))
+        .andExpect(
+            jsonPath("$['content'][0]['reviewId']")
+                .value(mocks.getContent().get(0).getReviewId().toString()))
+        .andExpect(
+            jsonPath("$['content'][0]['userId']")
+                .value(mocks.getContent().get(0).getUserId().toString()))
+        .andExpect(
+            jsonPath("$['content'][0]['point']")
+                .value(mocks.getContent().get(0).getPoint().toString()))
+        .andExpect(
+            jsonPath("$['content'][0]['placeId']")
+                .value(mocks.getContent().get(0).getPlaceId().toString()));
   }
 }
