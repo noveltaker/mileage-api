681a36f6cf07d536c030ef4891f812bc2b75984c
noveltaker <pnw123456@icloud.com>
2022-06-27 14:39:28 +0900

    fix: dto 400 error fixed

--- src/main/java/com/example/mileageapi/service/dto/MileageHistoryDTO.java
+++ src/main/java/com/example/mileageapi/service/dto/MileageHistoryDTO.java
@@ -3,12 +3,14 @@ package com.example.mileageapi.service.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.domain.PageRequest;
 
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class MileageHistoryDTO {
