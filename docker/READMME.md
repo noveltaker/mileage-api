b9d6a1f2eac9dff1a00341da8c53b911637223bf
noveltaker <pnw123456@icloud.com>
2022-06-27 10:02:20 +0900

    build: mysql add

---
+++ docker/READMME.md
@@ -0,0 +1,7 @@
+```
+docker run --name mysql_custom -d -p 3306:3306 mysql_custom:laster
+```
+
+```
+docker build -t mysql_custom:laster ./docker/.
+```
--- src/main/java/com/example/mileageapi/config/H2Configuration.java
+++ src/main/java/com/example/mileageapi/config/H2Configuration.java
@@ -5,17 +5,19 @@ import org.h2.tools.Server;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.sql.SQLException;
 
+@Profile("h2")
 @Configuration
 public class H2Configuration {
 
-    @Bean
-    @ConfigurationProperties("spring.datasource.hikari")
-    public HikariDataSource dataSource() throws SQLException {
-        Server.createTcpServer("-tcp", "-tcpAllowOthers", "-ifNotExists", "-tcpPort", 9095 + "")
-                .start();
-        return new HikariDataSource();
-    }
+  @Bean
+  @ConfigurationProperties("spring.datasource.hikari")
+  public HikariDataSource dataSource() throws SQLException {
+    Server.createTcpServer("-tcp", "-tcpAllowOthers", "-ifNotExists", "-tcpPort", 9095 + "")
+        .start();
+    return new HikariDataSource();
+  }
 }
